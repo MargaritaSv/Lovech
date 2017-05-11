f1fd77dd768c198528d7dcd7116d56bd51dfd675
MargaritaSv <margarita.esti@gmail.com>
2017-05-11 15:51:11 +0300

    UnitTest for production

---
+++ TestProducion.java
@@ -0,0 +1,20 @@
+package project1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestProducion {
+
+	// private ProductionDAO prodDAO
+	@Test
+	public void testProduction() {
+		//fail("Not yet implemented");
+		ProductionDAO dasada =new ProductionDAO();
+			dasada.registerProduction(new Production(5, "English weekend", "Autor","description", "actor7 , actor 2,actor3"));
+	
+	//yveri se, che..
+			//assert.
+	}
+
+}
