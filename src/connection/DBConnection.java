9fe99069e61ddb029c6bf43f12f0454a9dc84dac
MargaritaSv <margarita.esti@gmail.com>
2017-06-15 00:39:05 +0300

    connection to db

---
+++ src/connection/DBConnection.java
@@ -0,0 +1,44 @@
+package connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Connection;
+
+public class DBConnection {
+
+	private static final String DB_HOSTNAME = "127.0.0.1";
+	private static final String DB_PORT = "3306";
+	private static final String DATABASE = "lovech_db";
+	private static final String DB_USER = "root";
+	private static final String DB_PASSWORD = "estifira74";
+
+	private static Connection con;
+
+	private static DBConnection theOnlyOne;
+
+	private DBConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			this.con = (Connection) DriverManager.getConnection(
+					"jdbc:mysql://" + DB_HOSTNAME + ":" + DB_PORT + "/" + DATABASE + "?useSSL=false", DB_USER,
+					DB_PASSWORD);
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static DBConnection getInstance() {
+		if (theOnlyOne == null) {
+			theOnlyOne = new DBConnection();
+		}
+
+		return theOnlyOne;
+	}
+
+	public Connection getConnection() {
+		return con;
+	}
+}
